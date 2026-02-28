958e1758497c656ffd572f8bfee4072ec870ad09
Deepesh <i-deepesh.h@corestack.io>
2026-02-28 12:13:30 +0530

    commit

--- tutorial/main.py
+++ tutorial/main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     uvicorn.run("app.app:app", host="0.0.0.0",port=8000, reload=True)
 
 
-    # .run means running a webserver. the term "app.app:app" means that we are running the app object from the app.py file which is inside the app folder. The host is set to "0.0.0.0" so that the server is accessible from any IP address, and the port is set to 8080. The reload=True flag enables auto-reloading when code changes are detected.
+# .run means running a webserver. the term "app.app:app" means that we are running the app object from the app.py file which is inside the app folder. The host is set to "0.0.0.0" so that the server is accessible from any IP address, and the port is set to 8080. The reload=True flag enables auto-reloading when code changes are detected.
